a17b26ebc0820165354d0491560b07eed38fa6ab
Janindu Rananwake <janindu93@gmail.com>
2017-08-20 19:56:04 +0530

    bug fixes

--- js/custom-animations.js
+++ js/custom-animations.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
         $isAnimatedEighth = $('.eighth .is-animated'),
         $isAnimatedNinth = $('.nineth .is-animated'),
         $isAnimatedTenth = $('.tenth .is-animated'),
-        $isAnimatedel11 = $('.eleventh .is-animated');
+        $isAnimatedEleventh = $('.eleventh .is-animated');
 
 
 
@@ -22,149 +22,129 @@ $(document).ready(function() {
         //to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
         css3: false,
         onLeave: function(index, nextIndex, direction) {
-
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
             /* this code is part of the onLeave callback */
             // Page 02
             if (index == 1 && nextIndex == 2 && direction == 'down') {
-                $isAnimatedSecond.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedSecond.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedSecond.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 3 && nextIndex == 2 && direction == 'up') {
-                $isAnimatedSecond.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedSecond.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedSecond.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
 
             // Page 03 
             if (index == 2 && nextIndex == 3 && direction == 'down') {
-                $isAnimatedThird.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedThird.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedThird.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 4 && nextIndex == 3 && direction == 'up') {
-                $isAnimatedThird.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedThird.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedThird.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
 
             // Page 04 
             if (index == 3 && nextIndex == 4 && direction == 'down') {
-                $isAnimatedFourth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedFourth.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedFourth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 5 && nextIndex == 4 && direction == 'up') {
-                $isAnimatedFourth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedFourth.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedFourth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
 
             // Page 05 
             if (index == 4 && nextIndex == 5 && direction == 'down') {
-                $isAnimatedFifth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedFifth.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedFifth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 6 && nextIndex == 5 && direction == 'up') {
-                $isAnimatedFifth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedFifth.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedFifth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
 
             // Page 06 
             if (index == 5 && nextIndex == 6 && direction == 'down') {
-                $isAnimatedSixth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedSixth.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedSixth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 7 && nextIndex == 6 && direction == 'up') {
-                $isAnimatedSixth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedSixth.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedSixth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
 
             // Page 07 
             if (index == 6 && nextIndex == 7 && direction == 'down') {
-                $isAnimatedSeventh.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedSeventh.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedSeventh.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 8 && nextIndex == 7 && direction == 'up') {
-                $isAnimatedSeventh.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedSeventh.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedSeventh.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
 
             // Page 08 
             if (index == 7 && nextIndex == 8 && direction == 'down') {
-                $isAnimatedEighth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedEighth.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedEighth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 9 && nextIndex == 8 && direction == 'up') {
-                $isAnimatedEighth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedEighth.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedEighth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
 
             // Page 09 
             if (index == 8 && nextIndex == 9 && direction == 'down') {
-                $isAnimatedNinth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedNinth.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedNinth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 10 && nextIndex == 9 && direction == 'up') {
-                $isAnimatedNinth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedNinth.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedNinth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
 
             // Page 10 
             if (index == 9 && nextIndex == 10 && direction == 'down') {
-                $isAnimatedTenth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedTenth.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedTenth.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 11 && nextIndex == 10 && direction == 'up') {
-                $isAnimatedTenth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimatedTenth.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedTenth.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
 
             // Page 11 
             if (index == 10 && nextIndex == 11 && direction == 'down') {
-                $isAnimated11.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimated11.removeClass('animated slideInUp');
-                }, 1750)
+                $isAnimatedEleventh.addClass('animated slideInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
             if (index == 12 && nextIndex == 11 && direction == 'up') {
-                $isAnimated11.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' });
-                setTimeout(function() {
-                    $isAnimated11.removeClass('animated fadeInUp');
-                }, 1750)
+                $isAnimatedEleventh.addClass('animated fadeInUp').css({ 'animation-delay': '.2s', 'animation-duration': '1.4s' }).one(animationEnd, function() {
+                    $(this).removeClass('animated ' + 'slideInUp');
+                });
             }
-        },
+        }
 
     });
 });
